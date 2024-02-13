c2ce391e8ba5640c11ba047e2aa41603fc1d7ae6
matteo <gorlamatteo01@gmail.com>
2024-02-13 21:26:58 +0100

    modify crossorigin

--- src/main/java/com/example/Hospital_menagement_System/controller/HospitalController.java
+++ src/main/java/com/example/Hospital_menagement_System/controller/HospitalController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhsot:3000")
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/patients")
 @RequiredArgsConstructor
